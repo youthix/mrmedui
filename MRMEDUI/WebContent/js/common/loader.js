3f3efc8cafb55d587bffebb45393f936dcaa3354
saurabh <inbox.saurabh@gmail.com>
2016-06-10 10:37:15 -0500

    updates

---
+++ MRMEDUI/WebContent/js/common/loader.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+$( document ).ready(function() {
+	alert("Hi")
+	$.getScript("constant.js");
+	$.getScript("url.js");
+	$.getScript("../controller/restcaller.js");
+	alert("Hi2")
+	
+});
