cb24a168f139faaba41135e3c64c1ac9c8e0264b
saurabh <inbox.saurabh@gmail.com>
2016-06-10 11:37:18 -0500

    update

--- MRMEDUI/WebContent/js/common/constant.js
+++ MRMEDUI/WebContent/js/common/constant.js
@@ -1,3 +1,13 @@
 /**
  * 
  */
+/*
+ * Service URLs
+ * */
+var url = { 
+  login : "http://localhost/mrmedservices/rest/service/login",
+  getUser : "http://localhost/mrmedservices/rest/service/get/users",
+  saveUser : "http://localhost/mrmedservices/rest/service/get/users",
+  getCustomer : "http://localhost/mrmedservices/rest/service/get/customer",
+  saveCustomer : "http://localhost/mrmedservices/rest/service/get/customer",
+}
--- MRMEDUI/WebContent/js/common/loader.js
+++ MRMEDUI/WebContent/js/common/loader.js
@@ -2,11 +2,22 @@
  * 
  */
 
-$( document ).ready(function() {
+/*$( document ).ready(function() {
 	alert("Hi")
 	$.getScript("constant.js");
 	$.getScript("url.js");
 	$.getScript("../controller/restcaller.js");
 	alert("Hi2")
 	
+});*/
+
+	
+	jQuery.getScript("constant.js")
+	.done(function() {
+		alert('success');
+		/* yay, all good, do something */
+	})
+	.fail(function() {
+		alert('failure');
+		/* boo, fall back to something else */
 });
