96f83ddf1557f4d3e1779867c68f0d4617e2ad33
Saurabh Dixit <saurabh.iips@gmail.com>
2016-06-11 14:10:18 +0530

    updated

--- MRMEDUI/WebContent/js/common/loader.js
+++ MRMEDUI/WebContent/js/common/loader.js
@@ -2,7 +2,7 @@
  * 
  */
 
-jQuery.getScript("constant.js")
+jQuery.getScript("/constant.js")
 	.done(function() {
 		alert('success');
 		/* yay, all good, do something */
@@ -12,5 +12,6 @@ jQuery.getScript("constant.js")
 		/* boo, fall back to something else */
 });
 
-jQuery.getScript("../libs/jquery/jquery-2.2.4.min.js");
 jQuery.getScript("../libs/bootstrap/bootstrap.min.js");
+
+jQuery.getScript("../model/loginM.js");
